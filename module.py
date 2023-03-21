3d0cde3e6b210bb0f3f77f3ededbf5b861112f2f
Bryan Frederick <bscf2789@gmail.com>
2023-03-21 18:41:34 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,17 @@
+import math
+
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+
+def squareFootage(height, length):
+    # height = int(input('What is the height: '))
+    # length = int(input('What is the length: '))
+    square = height * length
+
+    print(square)
+
+def circumferenceOfCircle(radius):
+    circum = 2 * math.pi * radius
+
+    print(circum)
+
